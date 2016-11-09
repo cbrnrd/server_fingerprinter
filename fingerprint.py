3dac361f041b22ae552c3d20ac31736b01c2c195
thecarterb <cbawsome77@gmail.com>
2016-11-08 19:28:24 -0500

    add more features and improved error catching

--- fingerprint.py
+++ fingerprint.py
@@ -5,6 +5,7 @@ from optparse import OptionParser
 
 parser = OptionParser(usage="usage: sudo %prog [options]")
 parser.add_option("-t", "--target", action="store", dest="target", type=str, help="Server to fingerprint")
+parser.add_option("-u", "--user-agent", action="store", dest="uagent", type=str, default="curl/7.37.0", help="The fake(or real) user agent to use. (defaults to \"curl/7.37.0\"")
 (options, args) = parser.parse_args()
 
 #handle colors
@@ -15,23 +16,34 @@ ENDC = "\033[0m"
 
 def printMsg(s):
     print(OK_BLUE + "[*]" + ENDC + " " + s)
+
 def printGood(s):
     print(OK_GREEN + "[*]" + ENDC + " " + s)
+
 def printErr(s):
     print(ERR + "[!]" + ENDC + " " + s)
 
 
-if len(args) == 1:
+if not options.target:
     printErr("Missing the required \'-t\' option")
     printErr("Exiting...")
-    exit(1)
 else:
     try:
+        printMsg("Trying to contact %s..." % options.target)
         request = urllib2.Request(options.target)
-        request.add_header('User-Agent', 'curl/7.37.0')
+        request.add_header('User-Agent', options.uagent)
         response = urllib2.urlopen(request)
-        printGood("Results brought back server type of: " + OK_GREEN + response.info().getheader('Server') + ENDC)
+        try:
+            printGood("Results brought back server type of: " + OK_GREEN + response.info().getheader('Server') + ENDC)
+        except TypeError as typeerr:
+            printErr("Server responded with no server type or a spoofed server type.")
+            printErr("Exiting...")
         exit(0)
-    except (ValueError, urllib2.URLError) as e:
-        printErr("Server didn't respond. Exiting...")
+    except urllib2.URLError as urlerr:
+        printErr("Server didn't respond. (check the URL)")
+        printErr("Exiting...")
+        exit(1)
+    except ValueError as valerr:
+        printErr("Please put in a valid url. (with https:// and www.)")
+        printErr("Exiting... ")
         exit(1)
