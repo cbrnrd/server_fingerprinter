8118338013edc622db115f623f0963caef088ad3
Carter <cbawsome77@gmail.com>
2016-11-11 09:30:57 -0500

    add -n nmap option
    might be buggy

--- fingerprint.py
+++ fingerprint.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 import urllib2
+from subprocess import call
 from optparse import OptionParser
 
 parser = OptionParser(usage="Usage: python fingerprint.py -t target [-u User-Agent]")
 parser.add_option("-t", "--target", action="store", dest="target", type=str, help="Server to fingerprint")
 parser.add_option("-u", "--user-agent", action="store", dest="uagent", type=str, default="curl/7.37.0", help="The fake(or real) user agent to use. (defaults to \"curl/7.37.0\")")
+parser.add_option("-n", "--nmap", action="store_true", dest="nmapScan", default=False, help="Perform an nmap OS scan on the target."
 (options, args) = parser.parse_args()
 
 # handle colors
@@ -34,6 +36,10 @@ else:
         response = urllib2.urlopen(request)
         try:
             printGood("Results brought back server type of: " + OK_GREEN + response.info().getheader('Server') + ENDC)
+            print "\n"
+            if options.nmapScan == True:
+                printMsg("Starting nmap scan, hold on...)
+                call(["sudo", "nmap", "-O", "-sV", options.target]) # nmap scan command
         except TypeError as typeerr: # if there is no response header
             printErr("Server responded with no server header.")
             printErr("Exiting...")
